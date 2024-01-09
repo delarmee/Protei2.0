e877a78625afaddd31dc71e08ff4d1d1ffb7ab40
delarmee <raskopina.nastia@yandex.ru>
2024-01-09 07:24:03 +0300

    Настройка окружения для разработки

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from linear_regression_logic import perform_linear_regression
 from plotting import plot_results
 
-file_path = 'C:/Users/Анастасия/time_messagees.txt'  # Путь к  файлу
+file_path = 'C:/Users/Анастасия/time_messagees.txt'  # Путь к файлу
 
 X_test, y_test, y_pred, mse, r2 = perform_linear_regression(file_path)
 
