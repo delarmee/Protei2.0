1111b44c8445055aea0093706b8c989ad690cda1
delarmee <raskopina.nastia@yandex.ru>
2024-01-09 09:03:54 +0300

    Настройка окружения для разработки

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from plotting import plot_results
 
 file_path = 'C:/Users/Анастасия/time_messagees.txt'  # Путь к файлу
 
+# Линейная регрессия
 X_test, y_test, y_pred, mse, r2 = perform_linear_regression(file_path)
 
+# Вывод MSE и R2
 print('Mean squared error: %.2f' % mse)
 print('Coefficient of determination: %.2f' % r2)
 
+# Отрисовка результатов
 plot_results(X_test, y_test, y_pred)
